41dc3a03533aa04c859c698d061eee19e695714c
Victor Golob <victor.x.golob.-nd@disney.com>
2018-05-31 00:23:26 -0300

    add react-router

--- reactify_client/src/App.js
+++ reactify_client/src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Home from './components/Home';
+import Reactify from './components/Reactify';
+
 import './App.css';
 
-import HashParams from './helpers/HashParams';
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-    let params = new URLSearchParams(window.location.search);
-    this.state = {
-        token : params.get('access_token')
-    };
-  }
 
   render() {
     return (
-      <div className="App">
-        {(this.state.token !== null)
-            ? `You're authenticated, your acces token is ${this.state.token}`
-            : `There's a problem with your authentication.
-            \nRun Reactify-Server first, then go to http://localhost:8888/`}
-      </div>
+      <Router className="App">
+          <div>
+              <Route exact path="/" component={Home}/>
+              <Route path="/reactify" component={Reactify}/>
+          </div>
+      </Router>
     );
   }
 }
